1085d1ccb6b56fdde8ca66ea4adde54edcafc0db
sharmag44 <sharmagshambhu44@gmail.com>
2021-04-12 22:29:41 +0530

    variable created in js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // jshint es6
 
-const musicContainer  =
+const musicContainer  = document.querySelector('.music-container');
+const play = document.querySelector('#play');
+const prev  = document.querySelector('#prev');
+const next  = document.querySelector('#next');
+const musicContainer  = document.querySelector('.music-container');
+const musicContainer  = document.querySelector('.music-container');
+const musicContainer  = document.querySelector('.music-container');
+const musicContainer  = document.querySelector('.music-container');
